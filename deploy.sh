289daace7cafa5113c71419fa174caac59fa0d8a
Jenny Tong <jen@firebase.com>
2015-02-06 19:45:19 -0800

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+targetDir=`basename "$PWD"`
+targetPath="~/mimming.com/presos/$targetDir"
+targetHost="mimming.com"
+
+echo "Deploying to $targetHost:$targetPath"
+rsync --exclude=.git --exclude=test -vaz ./* $targetHost:$targetPath
